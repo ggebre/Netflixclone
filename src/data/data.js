ce7fd3c5a18a1289b86bef72a560e337cf17b999
Getu Gebre <getu.gebre.gg@gmail.com>
2021-06-10 10:17:08 -0500

    data created

---
+++ src/data/data.js
@@ -0,0 +1,256 @@
+const FOOLS_RUSH_IN = require('../../assets/images/Fools-Rush_In.jpeg')
+const THE_TAKE = require('../../assets/images/The-Take.jpeg')
+const ANGEL_HAS_FALLEN = require('../../assets/images/Angel-Has-Fallen.jpeg')
+const DUE_DATE = require('../../assets/images/Due-Date.jpeg')
+const CASINO_ROYALE = require('../../assets/images/Casion-Royale.jpeg')
+
+const SHOWS = [
+    {
+        title: 'Fools Rush In',
+        summary: "Three months after a one-night stand with Isabel Fuentes (Salma Hayek) in Las Vegas, New York City real estate developer Alex Whitman (Matthew Perry) learns she is pregnant",
+        starring: "Matthew Perry, Salma Hayek, Jon Tenney, Jill Clayburgh",
+        creator: "Katherine Reback",
+        image: FOOLS_RUSH_IN
+    },
+    {
+        title: 'The Take',
+        summary: "A rogue CIA agent (Idris Elba) forms an unlikely partnership with a pickpocket (Richard Madden) to stop a terrorist conspiracy in Paris.",
+        starring: "Idris Elba, Richard Madden, Charlotte Le Bon",
+        creator: "Andrew Baldwin, James Watkins",
+        image: THE_TAKE
+    },
+    {
+        title: 'Angel Has Fallen',
+        summary: "Mike Banning is framed for the attempted assassination of the President and must evade his own agency and the FBI as he tries to uncover the real threat.",
+        starring: "Gerard Butler, Frederick Schmidt, Danny Huston",
+        creator: " Robert Mark Kamen",
+        image: ANGEL_HAS_FALLEN
+    },
+    {
+        title: 'Due Date',
+        summary: "High-strung father-to-be Peter Highman is forced to hitch a ride with aspiring actor Ethan Tremblay on a road trip in order to make it to his child's birth on time.",
+        starring: " Robert Downey Jr., Zach Galifianakis, Michelle Monaghan",
+        creator: "Alan R. Cohen (screenplay), Alan Freedland (screenplay) ",
+        image: DUE_DATE
+    },
+    {
+        title: 'Casion Royale',
+        summary: "After earning 00 status and a licence to kill, Secret Agent James Bond sets out on his first mission as 007. Bond must defeat a private banker funding terrorists in a high-stakes game of poker at Casino Royale, Montenegro.",
+        starring: " Daniel Craig, Eva Green, Judi Dench ",
+        creator: " Neal Purvis (screenplay), Robert Wade (screenplay) ",
+        image: CASINO_ROYALE
+    },
+    {
+        title: 'Fools Rush In',
+        summary: "Three months after a one-night stand with Isabel Fuentes (Salma Hayek) in Las Vegas, New York City real estate developer Alex Whitman (Matthew Perry) learns she is pregnant",
+        starring: "Matthew Perry, Salma Hayek, Jon Tenney, Jill Clayburgh",
+        creator: "Katherine Reback",
+        image: FOOLS_RUSH_IN
+    },
+    {
+        title: 'The Take',
+        summary: "A rogue CIA agent (Idris Elba) forms an unlikely partnership with a pickpocket (Richard Madden) to stop a terrorist conspiracy in Paris.",
+        starring: "Idris Elba, Richard Madden, Charlotte Le Bon",
+        creator: "Andrew Baldwin, James Watkins",
+        image: THE_TAKE
+    },
+    {
+        title: 'Angel Has Fallen',
+        summary: "Mike Banning is framed for the attempted assassination of the President and must evade his own agency and the FBI as he tries to uncover the real threat.",
+        starring: "Gerard Butler, Frederick Schmidt, Danny Huston",
+        creator: " Robert Mark Kamen",
+        image: ANGEL_HAS_FALLEN
+    },
+    {
+        title: 'Due Date',
+        summary: "High-strung father-to-be Peter Highman is forced to hitch a ride with aspiring actor Ethan Tremblay on a road trip in order to make it to his child's birth on time.",
+        starring: " Robert Downey Jr., Zach Galifianakis, Michelle Monaghan",
+        creator: "Alan R. Cohen (screenplay), Alan Freedland (screenplay) ",
+        image: DUE_DATE
+    },
+    {
+        title: 'Casion Royale',
+        summary: "After earning 00 status and a licence to kill, Secret Agent James Bond sets out on his first mission as 007. Bond must defeat a private banker funding terrorists in a high-stakes game of poker at Casino Royale, Montenegro.",
+        starring: " Daniel Craig, Eva Green, Judi Dench ",
+        creator: " Neal Purvis (screenplay), Robert Wade (screenplay) ",
+        image: CASINO_ROYALE
+    },
+    {
+        title: 'Fools Rush In',
+        summary: "Three months after a one-night stand with Isabel Fuentes (Salma Hayek) in Las Vegas, New York City real estate developer Alex Whitman (Matthew Perry) learns she is pregnant",
+        starring: "Matthew Perry, Salma Hayek, Jon Tenney, Jill Clayburgh",
+        creator: "Katherine Reback",
+        image: FOOLS_RUSH_IN
+    },
+    {
+        title: 'The Take',
+        summary: "A rogue CIA agent (Idris Elba) forms an unlikely partnership with a pickpocket (Richard Madden) to stop a terrorist conspiracy in Paris.",
+        starring: "Idris Elba, Richard Madden, Charlotte Le Bon",
+        creator: "Andrew Baldwin, James Watkins",
+        image: THE_TAKE
+    },
+    {
+        title: 'Angel Has Fallen',
+        summary: "Mike Banning is framed for the attempted assassination of the President and must evade his own agency and the FBI as he tries to uncover the real threat.",
+        starring: "Gerard Butler, Frederick Schmidt, Danny Huston",
+        creator: " Robert Mark Kamen",
+        image: ANGEL_HAS_FALLEN
+    },
+    {
+        title: 'Due Date',
+        summary: "High-strung father-to-be Peter Highman is forced to hitch a ride with aspiring actor Ethan Tremblay on a road trip in order to make it to his child's birth on time.",
+        starring: " Robert Downey Jr., Zach Galifianakis, Michelle Monaghan",
+        creator: "Alan R. Cohen (screenplay), Alan Freedland (screenplay) ",
+        image: DUE_DATE
+    },
+    {
+        title: 'Casion Royale',
+        summary: "After earning 00 status and a licence to kill, Secret Agent James Bond sets out on his first mission as 007. Bond must defeat a private banker funding terrorists in a high-stakes game of poker at Casino Royale, Montenegro.",
+        starring: " Daniel Craig, Eva Green, Judi Dench ",
+        creator: " Neal Purvis (screenplay), Robert Wade (screenplay) ",
+        image: CASINO_ROYALE
+    },
+    {
+        title: 'Fools Rush In',
+        summary: "Three months after a one-night stand with Isabel Fuentes (Salma Hayek) in Las Vegas, New York City real estate developer Alex Whitman (Matthew Perry) learns she is pregnant",
+        starring: "Matthew Perry, Salma Hayek, Jon Tenney, Jill Clayburgh",
+        creator: "Katherine Reback",
+        image: FOOLS_RUSH_IN
+    },
+    {
+        title: 'The Take',
+        summary: "A rogue CIA agent (Idris Elba) forms an unlikely partnership with a pickpocket (Richard Madden) to stop a terrorist conspiracy in Paris.",
+        starring: "Idris Elba, Richard Madden, Charlotte Le Bon",
+        creator: "Andrew Baldwin, James Watkins",
+        image: THE_TAKE
+    },
+    {
+        title: 'Angel Has Fallen',
+        summary: "Mike Banning is framed for the attempted assassination of the President and must evade his own agency and the FBI as he tries to uncover the real threat.",
+        starring: "Gerard Butler, Frederick Schmidt, Danny Huston",
+        creator: " Robert Mark Kamen",
+        image: ANGEL_HAS_FALLEN
+    },
+    {
+        title: 'Due Date',
+        summary: "High-strung father-to-be Peter Highman is forced to hitch a ride with aspiring actor Ethan Tremblay on a road trip in order to make it to his child's birth on time.",
+        starring: " Robert Downey Jr., Zach Galifianakis, Michelle Monaghan",
+        creator: "Alan R. Cohen (screenplay), Alan Freedland (screenplay) ",
+        image: DUE_DATE
+    },
+    {
+        title: 'Casion Royale',
+        summary: "After earning 00 status and a licence to kill, Secret Agent James Bond sets out on his first mission as 007. Bond must defeat a private banker funding terrorists in a high-stakes game of poker at Casino Royale, Montenegro.",
+        starring: " Daniel Craig, Eva Green, Judi Dench ",
+        creator: " Neal Purvis (screenplay), Robert Wade (screenplay) ",
+        image: CASINO_ROYALE
+    },
+    {
+        title: 'Fools Rush In',
+        summary: "Three months after a one-night stand with Isabel Fuentes (Salma Hayek) in Las Vegas, New York City real estate developer Alex Whitman (Matthew Perry) learns she is pregnant",
+        starring: "Matthew Perry, Salma Hayek, Jon Tenney, Jill Clayburgh",
+        creator: "Katherine Reback",
+        image: FOOLS_RUSH_IN
+    },
+    {
+        title: 'The Take',
+        summary: "A rogue CIA agent (Idris Elba) forms an unlikely partnership with a pickpocket (Richard Madden) to stop a terrorist conspiracy in Paris.",
+        starring: "Idris Elba, Richard Madden, Charlotte Le Bon",
+        creator: "Andrew Baldwin, James Watkins",
+        image: THE_TAKE
+    },
+    {
+        title: 'Angel Has Fallen',
+        summary: "Mike Banning is framed for the attempted assassination of the President and must evade his own agency and the FBI as he tries to uncover the real threat.",
+        starring: "Gerard Butler, Frederick Schmidt, Danny Huston",
+        creator: " Robert Mark Kamen",
+        image: ANGEL_HAS_FALLEN
+    },
+    {
+        title: 'Due Date',
+        summary: "High-strung father-to-be Peter Highman is forced to hitch a ride with aspiring actor Ethan Tremblay on a road trip in order to make it to his child's birth on time.",
+        starring: " Robert Downey Jr., Zach Galifianakis, Michelle Monaghan",
+        creator: "Alan R. Cohen (screenplay), Alan Freedland (screenplay) ",
+        image: DUE_DATE
+    },
+    {
+        title: 'Casion Royale',
+        summary: "After earning 00 status and a licence to kill, Secret Agent James Bond sets out on his first mission as 007. Bond must defeat a private banker funding terrorists in a high-stakes game of poker at Casino Royale, Montenegro.",
+        starring: " Daniel Craig, Eva Green, Judi Dench ",
+        creator: " Neal Purvis (screenplay), Robert Wade (screenplay) ",
+        image: CASINO_ROYALE
+    },
+    {
+        title: 'Fools Rush In',
+        summary: "Three months after a one-night stand with Isabel Fuentes (Salma Hayek) in Las Vegas, New York City real estate developer Alex Whitman (Matthew Perry) learns she is pregnant",
+        starring: "Matthew Perry, Salma Hayek, Jon Tenney, Jill Clayburgh",
+        creator: "Katherine Reback",
+        image: FOOLS_RUSH_IN
+    },
+    {
+        title: 'The Take',
+        summary: "A rogue CIA agent (Idris Elba) forms an unlikely partnership with a pickpocket (Richard Madden) to stop a terrorist conspiracy in Paris.",
+        starring: "Idris Elba, Richard Madden, Charlotte Le Bon",
+        creator: "Andrew Baldwin, James Watkins",
+        image: THE_TAKE
+    },
+    {
+        title: 'Angel Has Fallen',
+        summary: "Mike Banning is framed for the attempted assassination of the President and must evade his own agency and the FBI as he tries to uncover the real threat.",
+        starring: "Gerard Butler, Frederick Schmidt, Danny Huston",
+        creator: " Robert Mark Kamen",
+        image: ANGEL_HAS_FALLEN
+    },
+    {
+        title: 'Due Date',
+        summary: "High-strung father-to-be Peter Highman is forced to hitch a ride with aspiring actor Ethan Tremblay on a road trip in order to make it to his child's birth on time.",
+        starring: " Robert Downey Jr., Zach Galifianakis, Michelle Monaghan",
+        creator: "Alan R. Cohen (screenplay), Alan Freedland (screenplay) ",
+        image: DUE_DATE
+    },
+    {
+        title: 'Casion Royale',
+        summary: "After earning 00 status and a licence to kill, Secret Agent James Bond sets out on his first mission as 007. Bond must defeat a private banker funding terrorists in a high-stakes game of poker at Casino Royale, Montenegro.",
+        starring: " Daniel Craig, Eva Green, Judi Dench ",
+        creator: " Neal Purvis (screenplay), Robert Wade (screenplay) ",
+        image: CASINO_ROYALE
+    },
+    {
+        title: 'Fools Rush In',
+        summary: "Three months after a one-night stand with Isabel Fuentes (Salma Hayek) in Las Vegas, New York City real estate developer Alex Whitman (Matthew Perry) learns she is pregnant",
+        starring: "Matthew Perry, Salma Hayek, Jon Tenney, Jill Clayburgh",
+        creator: "Katherine Reback",
+        image: FOOLS_RUSH_IN
+    },
+    {
+        title: 'The Take',
+        summary: "A rogue CIA agent (Idris Elba) forms an unlikely partnership with a pickpocket (Richard Madden) to stop a terrorist conspiracy in Paris.",
+        starring: "Idris Elba, Richard Madden, Charlotte Le Bon",
+        creator: "Andrew Baldwin, James Watkins",
+        image: THE_TAKE
+    },
+    {
+        title: 'Angel Has Fallen',
+        summary: "Mike Banning is framed for the attempted assassination of the President and must evade his own agency and the FBI as he tries to uncover the real threat.",
+        starring: "Gerard Butler, Frederick Schmidt, Danny Huston",
+        creator: " Robert Mark Kamen",
+        image: ANGEL_HAS_FALLEN
+    },
+    {
+        title: 'Due Date',
+        summary: "High-strung father-to-be Peter Highman is forced to hitch a ride with aspiring actor Ethan Tremblay on a road trip in order to make it to his child's birth on time.",
+        starring: " Robert Downey Jr., Zach Galifianakis, Michelle Monaghan",
+        creator: "Alan R. Cohen (screenplay), Alan Freedland (screenplay) ",
+        image: DUE_DATE
+    },
+    {
+        title: 'Casion Royale',
+        summary: "After earning 00 status and a licence to kill, Secret Agent James Bond sets out on his first mission as 007. Bond must defeat a private banker funding terrorists in a high-stakes game of poker at Casino Royale, Montenegro.",
+        starring: " Daniel Craig, Eva Green, Judi Dench ",
+        creator: " Neal Purvis (screenplay), Robert Wade (screenplay) ",
+        image: CASINO_ROYALE
+    },
+]
+
+
+export default SHOWS 
