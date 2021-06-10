296995300fd3804d7e8b0bc31eb7becd31f487dd
Getu Gebre <getu.gebre.gg@gmail.com>
2021-06-10 11:00:10 -0500

    create header component

--- babel.config.js
+++ babel.config.js
@@ -2,15 +2,15 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    env: {
-      development: {
-        plugins: [
-          "transform-react-jsx-source",
-          ["module-resolver", {
-            "root": ["./src"]
-          }]
-        ]
-      }
-    }
+    // env: {
+    //   development: {
+    //     plugins: [
+    //       "transform-react-jsx-source",
+    //       ["module-resolver", {
+    //         "root": ["./src"]
+    //       }]
+    //     ]
+    //   }
+    // }
   };
 };
